4ed90419dee8a667722dd1d10157684895a7c2dc
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2025-05-19 07:09:10 -0300

    DB-feature

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -19,6 +19,6 @@ export class CreateUserDto {
 
     @IsString()
     @MinLength(1)
-    fullName: string;
+    name: string;
 
 }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -19,7 +19,7 @@ export class User {
     password: string;
 
     @Column('text')
-    fullName: string;
+    name: string;
 
     @Column('bool', { default: true })
     isActive: boolean;
