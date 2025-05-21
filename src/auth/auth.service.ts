773b1d7174d929f46546249b4c0d6fddd5e23aee
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2025-05-21 06:33:34 -0300

    v3

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -57,7 +57,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true, id: true }
+      select: { email: true, id: true, name: true, roles: true }
     });
 
     if (!user)
